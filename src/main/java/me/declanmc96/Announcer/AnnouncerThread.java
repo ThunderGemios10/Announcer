2717cbbfa92bd7c081c3b4a441aab0adc092a833
Romarjosh Villamor <marilyndris@gmail.com>
2016-01-27 06:29:04 -0800

    Thread

---
+++ src/main/java/me/declanmc96/Announcer/AnnouncerThread.java
@@ -0,0 +1,31 @@
+package me.declanmc96.Announcer;
+
+import java.util.Random;
+
+public class AnnouncerThread
+  extends Thread
+{
+  private final Random randomGenerator = new Random();
+  private final announcer plugin;
+  private int lastAnnouncement = 0;
+  
+  public AnnouncerThread(Announcer plugin)
+  {
+    this.plugin = plugin;
+  }
+  
+  public void run()
+  {
+    if (this.plugin.isAnnouncerEnabled())
+    {
+      if (this.plugin.isRandom()) {
+        this.lastAnnouncement = Math.abs(this.randomGenerator.nextInt() % this.plugin.numberOfAnnouncements());
+      } else if (++this.lastAnnouncement >= this.plugin.numberOfAnnouncements()) {
+        this.lastAnnouncement = 0;
+      }
+      if (this.lastAnnouncement < this.plugin.numberOfAnnouncements()) {
+        this.plugin.announce(this.lastAnnouncement + 1);
+      }
+    }
+  }
+}
