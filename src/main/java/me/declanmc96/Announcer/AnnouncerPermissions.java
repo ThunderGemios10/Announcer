e49d8f3e087145c1c123665f03443afc639d78da
Romarjosh Villamor <marilyndris@gmail.com>
2016-01-27 06:27:51 -0800

    Permissions

---
+++ src/main/java/me/declanmc96/Announcer/AnnouncerPermissions.java
@@ -0,0 +1,11 @@
+package me.declanmc96.Announcer;
+
+public final class AnnouncerPermissions
+{
+  public static final String RECEIVER = "announcer.receiver";
+  public static final String ADD = "announcer.add";
+  public static final String DELETE = "announcer.delete";
+  public static final String BROADCAST = "announcer.broadcast";
+  public static final String MODERATOR = "announcer.moderate";
+  public static final String ADMINISTRATOR = "announcer.admin";
+}
