5e57bc27702bfa4edc59725db46b84272c1cb1ac
Romarjosh Villamor <marilyndris@gmail.com>
2016-01-28 04:40:26 -0800

    Fixed an error

--- src/main/java/me/declanmc96/Announcer/ChatColorHelper.java
+++ src/main/java/me/declanmc96/Announcer/ChatColorHelper.java
@@ -1,14 +1,15 @@
 package me.declanmc96.Announcer;
-
+ 
 import org.bukkit.ChatColor;
-
+ 
 public class ChatColorHelper
 {
   public static String replaceColorCodes(String message)
   {
-    for (ChatColor color : ) {
+    for (ChatColor color : ChatColor.values()) {
       message = message.replaceAll(String.format("&%c", new Object[] { Character.valueOf(color.getChar()) }), color.toString());
     }
+ 
     return message;
   }
 }
